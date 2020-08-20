9a09bfa44ba5f969bb7c7fc1b06bf1c2376b583d
Kevin Bui <kbui1993@gmail.com>
2020-08-19 22:29:16 -0700

    Uploaded
    code uploaded for first time.

---
+++ README.md
@@ -0,0 +1,55 @@
+# Nonconvex Network Slimming (Pytorch)
+
+This repository is an extension of the repository of [Network Slimming (Pytorch)](https://github.com/Eric-mingjie/network-slimming), an official pytorch implementation of the following paper:
+[Learning Efficient Convolutional Networks Through Network Slimming](http://openaccess.thecvf.com/content_iccv_2017/html/Liu_Learning_Efficient_Convolutional_ICCV_2017_paper.html) (ICCV 2017).  
+[Zhuang Liu](https://liuzhuang13.github.io/), [Jianguo Li](https://sites.google.com/site/leeplus/), [Zhiqiang Shen](http://zhiqiangshen.com/), [Gao Huang](http://www.cs.cornell.edu/~gaohuang/), [Shoumeng Yan](https://scholar.google.com/citations?user=f0BtDUQAAAAJ&hl=en), [Changshui Zhang](http://bigeye.au.tsinghua.edu.cn/english/Introduction.html).  
+
+It incorporates L_p, 0 < p < 1, and transformed L1 for nonconvex regularization of the channel scores. In addition, the dataset SVHN is available to train on. 
+
+Citation:
+```
+@InProceedings{Liu_2017_ICCV,
+    author = {Liu, Zhuang and Li, Jianguo and Shen, Zhiqiang and Huang, Gao and Yan, Shoumeng and Zhang, Changshui},
+    title = {Learning Efficient Convolutional Networks Through Network Slimming},
+    booktitle = {The IEEE International Conference on Computer Vision (ICCV)},
+    month = {Oct},
+    year = {2017}
+}
+```
+
+
+## Dependencies
+torch v0.3.1, torchvision v0.2.0
+
+## Channel Selection Layer
+We introduce `channel selection` layer to help the  pruning of ResNet and DenseNet. This layer is easy to implement. It stores a parameter `indexes` which is initialized to an all-1 vector. During pruning, it will set some places to 0 which correspond to the pruned channels.
+
+## Baseline 
+
+The `dataset` argument specifies which dataset to use: `cifar10`, `cifar100`, or `SVHN` The `arch` argument specifies the architecture to use: `vgg`,`resnet` or
+`densenet`. The depth is chosen to be the same as the networks used in the paper.
+```shell
+python main.py --dataset cifar10 --arch vgg --depth 19
+```
+
+## Train with Sparsity
+
+The `reg` argument specifies which regularization to use: `L1`, `TL1`, or `Lp`. The `a` argument specifies the nonconvex parameter. In particular, for Lp regularization, `a` has to have values strictly between 0 and 1; for TL1, `a` has to have values greater than 0.
+```shell
+python main.py -sr --s 0.0001 --dataset cifar10 --arch vgg --depth 19 --reg L1
+python main.py -sr --s 0.0001 --dataset cifar10 --arch vgg --depth 19 --reg Lp --a 0.5
+python main.py -sr --s 0.0001 --dataset cifar10 --arch vgg --depth 19 --reg TL1 --a 1.0
+```
+
+## Prune
+
+```shell
+python vggprune.py --dataset cifar10 --depth 19 --percent 0.7 --model [PATH TO THE MODEL] --save [DIRECTORY TO STORE RESULT]
+```
+The pruned model will be named `pruned.pth.tar`.
+
+## Fine-tune
+
+```shell
+python main.py --refine [PATH TO THE PRUNED MODEL] --dataset cifar10 --arch vgg --depth 19 --epochs 160
+```
---
+++ main.py
@@ -0,0 +1,256 @@
+from __future__ import print_function
+import os
+import argparse
+import shutil
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import pandas as pd
+import numpy as np
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+import models
+
+
+def appendDFToCSV_void(df, csvFilePath, sep=","):
+    import os
+    if not os.path.isfile(csvFilePath):
+        df.to_csv(csvFilePath, mode='a', index=False, sep=sep)
+    elif len(df.columns) != len(pd.read_csv(csvFilePath, nrows=1, sep=sep).columns):
+        raise Exception("Columns do not match!! Dataframe has " + str(len(df.columns)) + " columns. CSV file has " + str(len(pd.read_csv(csvFilePath, nrows=1, sep=sep).columns)) + " columns.")
+    elif not (df.columns == pd.read_csv(csvFilePath, nrows=1, sep=sep).columns).all():
+        raise Exception("Columns and column order of dataframe and csv file do not match!!")
+    else:
+        df.to_csv(csvFilePath, mode='a', index=False, sep=sep, header=False)
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch Slimming CIFAR training')
+parser.add_argument('--dataset', type=str, default='cifar100',
+                    help='training dataset (default: cifar100)')
+parser.add_argument('--sparsity-regularization', '-sr', dest='sr', action='store_true',
+                    help='train with channel sparsity regularization')
+parser.add_argument('--s', type=float, default=0.0001,
+                    help='scale sparse rate (default: 0.0001)')
+parser.add_argument('--refine', default='', type=str, metavar='PATH',
+                    help='path to the pruned model to be fine tuned')
+parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--test-batch-size', type=int, default=64, metavar='N',
+                    help='input batch size for testing (default: 256)')
+parser.add_argument('--epochs', type=int, default=160, metavar='N',
+                    help='number of epochs to train (default: 160)')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
+                    help='learning rate (default: 0.1)')
+parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                    help='SGD momentum (default: 0.9)')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--name', default='L0WideResNet', type=str,
+                    help='name of experiment')
+parser.add_argument('--log-interval', type=int, default=100, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--save', default='./logs', type=str, metavar='PATH',
+                    help='path to save prune model (default: current directory)')
+parser.add_argument('--arch', default='vgg', type=str, 
+                    help='architecture to use')
+parser.add_argument('--reg', default='L1', type=str,
+                    help='regularization method')
+parser.add_argument('--a', default = 1.0, type=float,
+                    help='additional parameter for nonconvex regularizer')
+parser.add_argument('--depth', default=19, type=int,
+                    help='depth of the neural network')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+args.name += '_{}_{}_{}_{}'.format(args.arch, args.depth, args.dataset, args.reg)
+
+if not os.path.exists(args.save):
+    os.makedirs(args.save)
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+if args.dataset == 'cifar10':
+    train_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('./data.cifar10', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.Pad(4),
+                           transforms.RandomCrop(32),
+                           transforms.RandomHorizontalFlip(),
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('./data.cifar10', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+elif args.dataset == 'SVHN':
+    train_loader = torch.utils.data.DataLoader(
+        datasets.SVHN('./data.SVHN', split = "train", download = True, transform =transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.SVHN('./data.SVHN', split = "test", download = True, transform = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                ])),
+        batch_size =args.test_batch_size, shuffle = True, **kwargs)
+elif args.dataset == 'cifar100':
+    train_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR100('./data.cifar100', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.Pad(4),
+                           transforms.RandomCrop(32),
+                           transforms.RandomHorizontalFlip(),
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR100('./data.cifar100', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+if args.refine:
+    checkpoint = torch.load(args.refine)
+    model = models.__dict__[args.arch](dataset=args.dataset, depth=args.depth, cfg=checkpoint['cfg'])
+    model.load_state_dict(checkpoint['state_dict'])
+    args.name = args.name+'_refine'
+else:
+    model = models.__dict__[args.arch](dataset=args.dataset, depth=args.depth)
+
+if args.cuda:
+    model.cuda()
+
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+
+if args.resume:
+    if os.path.isfile(args.resume):
+        print("=> loading checkpoint '{}'".format(args.resume))
+        checkpoint = torch.load(args.resume)
+        args.start_epoch = checkpoint['epoch']
+        best_prec1 = checkpoint['best_prec1']
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        print("=> loaded checkpoint '{}' (epoch {}) Prec1: {:f}"
+              .format(args.resume, checkpoint['epoch'], best_prec1))
+    else:
+        print("=> no checkpoint found at '{}'".format(args.resume))
+
+# additional subgradient descent on the sparsity-induced penalty term
+def updateBN():
+    for m in model.modules():
+        if isinstance(m, nn.BatchNorm2d):
+            m.weight.grad.data.add_(args.s*torch.sign(m.weight.data))  # L1
+
+def updateTL1BN(a):
+    for m in model.modules():
+        if isinstance(m, nn.BatchNorm2d):
+            m.weight.grad.data.add_(args.s*a*(a+1)*torch.sign(m.weight.data)/(a+m.weight.data.abs())**2)  #TL1
+
+def updateLp(a):
+    for m in model.modules():
+        if isinstance(m, nn.BatchNorm2d):
+            m.weight.grad.data.add_(args.s*(a*torch.sign(m.weight.data)/(m.weight.data.abs()**(1-a))))
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+        pred = output.data.max(1, keepdim=True)[1]
+        loss.backward()
+        if args.sr:
+            if args.reg == 'L1':
+                updateBN()
+            elif args.reg == 'TL1':
+                updateTL1BN(args.a)
+            elif args.reg == 'L1L2':
+                updateL1L2BN(args.a)
+            elif args.reg == 'SCAD':
+                updateSCADBN(args.a)
+            elif args.reg == 'L12':
+                updateL1_2()
+            elif args.reg == 'Lp':
+                updateLp(args.a)
+
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.1f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = model(data)
+        test_loss += F.cross_entropy(output, target, size_average=False).data.item() # sum up batch loss
+        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.1f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct.float()*1.0 / len(test_loader.dataset)))
+    return correct.float()*1.0 / float(len(test_loader.dataset))
+
+def save_checkpoint(state, is_best, filepath):
+    torch.save(state, os.path.join(filepath, args.name+'checkpoint.pth.tar'))
+    if is_best:
+        shutil.copyfile(os.path.join(filepath, args.name+'checkpoint.pth.tar'), os.path.join(filepath, args.name+'model_best.pth.tar'))
+
+best_prec1 = 0.
+for epoch in range(args.start_epoch, args.epochs):
+    if epoch in [args.epochs*0.5, args.epochs*0.75]:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] *= 0.1
+    train(epoch)
+    prec1 = test()
+    is_best = prec1 > best_prec1
+    best_prec1 = max(prec1, best_prec1)
+    save_checkpoint({
+        'epoch': epoch + 1,
+        'state_dict': model.state_dict(),
+        'best_prec1': best_prec1,
+        'optimizer': optimizer.state_dict(),
+    }, is_best, filepath=args.save)
+
+print("Best accuracy: "+str(best_prec1.item()))
+
+if args.refine:
+    data = [args.name, args.arch, args.depth, args.dataset, best_prec1.item()]
+    info_df = pd.DataFrame(data)
+    info_df = info_df.transpose()
+    info_df.columns =  ['Name', 'Architecture', 'Depth', 'Dataset', 'Best Accuracy']
+
+    appendDFToCSV_void(info_df, os.path.join(args.save, 'refine_record.csv'))
+else:
+    data = [args.name, args.arch, args.depth, args.dataset, args.reg, args.a, args.s, best_prec1.item()]
+    info_df = pd.DataFrame(data)
+    info_df = info_df.transpose()
+    info_df.columns =  ['Name', 'Architecture', 'Depth', 'Dataset', 'Regularizer', 'Reg Parameter', 'Sparsity Parameter', 'Best Accuracy']
+
+    appendDFToCSV_void(info_df, os.path.join(args.save, 'record.csv'))
